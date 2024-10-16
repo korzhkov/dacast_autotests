776c7d3d98a65c98ea3f304eeaadcfb1627454eb
korzhkov <korzhkov@gmail.com>
2024-10-16 15:12:55 +0100

    x3

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,6 @@
-   nvm use 16
-   xvfb-run npm run test:all:startfrom -- test:analytics
+   
+cd "$(dirname "$0")"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm use 16
+xvfb-run npm run test:all:startfrom -- test:analytics
